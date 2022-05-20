d6aee2ffcada1b5101fa2b15bc1cdca1a84bb1d2
DESKTOP-I33U0D5\dorat <iis20066@uom.edu.gr>
2022-05-20 15:42:48 +0300

    R6 screen

---
+++ app/src/main/java/com/example/airball/CompletedMatchStatistics.java
@@ -0,0 +1,14 @@
+package com.example.airball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompletedMatchStatistics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_completed_match_statistics);
+    }
+}
